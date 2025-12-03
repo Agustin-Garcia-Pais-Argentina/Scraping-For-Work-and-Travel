7436c63c67f09f36171f53b2d71e4f825580525a
AgustinGarciaUTN <aggarcia@frsf.utn.edu.ar>
2025-12-03 02:06:53 -0300

    mejor

--- handlers/vuelos.py
+++ handlers/vuelos.py
@@ -1,6 +1,9 @@
 from aiogram.types import Message
-from scraping import flights
-from scraping.flights import get_flight_price
+from scraping.flights import FlightScraper
+import datetime
+import random
+
+
 
 
 async def handle_vuelos(message: Message):
@@ -12,14 +15,21 @@ async def handle_vuelos(message: Message):
     elif 'driggs' in query.lower():
         origin = 'JAC'
 
-
-    # buscar JAC -> EZE (Buenos Aires EZE)
-    flights = get_flight_price(origin, 'EZE')
-    if not flights:
+    start = datetime.date(2026, 3, 15)
+    end = datetime.date(2026, 3, 30)
+    delta_days = (end - start).days
+    scraper = FlightScraper()
+    chosen_date = start + datetime.timedelta(days=random.randint(0, delta_days))
+    date_str = chosen_date.strftime("%Y-%m-%d")
+    
+    scraper = FlightScraper()
+    price, best = scraper.get_flight_price(origin, 'EZE', date_str)
+    
+    
+    if not price:
         await message.answer("No encontré vuelos ahora mismo.")
         return
 
-    best = flights[0]
     text = (
     f"✈️ *Mejor opción {origin}->EZE*\n"
     f"Precio: ${best['price']}\n"
